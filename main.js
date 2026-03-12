2197a6d12ec0910d61708db1db90c1735a5f0d2c
lilscribby <33818074+lilscribby@users.noreply.github.com>
2026-03-12 10:48:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `welcome, ${myName}`;
   }
 }
 
